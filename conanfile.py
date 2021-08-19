29ac69c17d74c06c917064f2988fbad2647a929a
regina-wakeup <88827164+regina-wakeup@users.noreply.github.com>
2021-08-19 23:09:13 +0700

    Add files via upload

---
+++ conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, CMake
+
+
+class HelloConan(ConanFile):
+    name = "hello"
+    generators = "cmake"
+    exports_sources = "lib/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="lib")
+        cmake.build()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="lib")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
+        if self.settings.compiler in ["gcc", "clang"]:
+            self.cpp_info.cxxflags = ["-std=c++17"]
