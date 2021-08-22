92b1c72e40b24b8235b17ca0ac93f998f5d76b31
regina-wakeup <88827164+regina-wakeup@users.noreply.github.com>
2021-08-22 15:04:45 +0700

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -24,7 +24,7 @@ class HelloConan(ConanFile):
             del self.options.fPIC
 
     def build(self):
-        os.environ["FIRST_TASK_VERSION"] = self.version
+        self.os.environ["FIRST_TASK_VERSION"] = self.version
         cmake = CMake(self)
         cmake.configure(source_folder="src")
         cmake.build()
